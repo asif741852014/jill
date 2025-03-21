729475b514376c8e21b6cf74ca27734a3c4ac9f1
asif741852014 <asiftkmohammed@gmail.com>
2025-03-22 02:53:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ module "iam" {
 module "eks" {
   source                = "./modules/eks"
   master_arn            = module.iam.master_arn
-  public_subnet_az1_id = module.iam.subnet_1
-  public_subnet_az2_id = module.iam.subnet_2
+  public_subnet_az1_id = module.vpc.subnet_1
+  public_subnet_az2_id = module.vpc.subnet_2
   worker_arn            = module.iam.worker_arn
   instance_size         = "t2.medium"
   eks_security_group_id = module.sg.sgid
